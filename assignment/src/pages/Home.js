4fa3c266cf69be2b9bf50b698cbf72bd5deffbc5
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-19 17:21:07 +0000

    set font size

--- assignment/src/pages/Home.js
+++ assignment/src/pages/Home.js
@@ -1,11 +1,27 @@
 import { students } from '../assets/StudentsDb.js'
 import StudentTable from '../components/StudentTable.js';
+import react,{useState} from 'react';
 
 export default function Home() {
 
+    const [fntSize, setFontSize] = useState(16);
+
     return (
-        <div className='outerDiv'>
-            <StudentTable stuDetails={students}/>   
-        </div>
+        <>
+            <center>
+                <h1>Student Information Portal</h1>
+            </center>
+            <hr></hr>
+            <div align="left" >
+                Font Size: 
+                <button onClick={()=>{setFontSize(16)}}>S</button>
+                <button onClick={()=>{setFontSize(20)}}>M</button>
+                <button onClick={()=>{setFontSize(24)}}>L</button>
+            </div>
+            <hr></hr>
+            <div className='outerDiv' style={{ fontSize: `${fntSize}px`}}>
+                <StudentTable stuDetails={students}/>   
+            </div>
+        </>
     )
 }
